9a89970242e8e2ca2fde4f98ad1137ef7e1d5d1f
SznImperial <adetolaabdulkareemayomide@gmail.com>
2025-08-17 06:59:49 +0100

    "Changes in responsiveness"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Footer = () => {
     return (
         <footer style={{ textAlign: "center", padding: "2rem 0", borderTop: "1px solid #00FF7F", marginTop: "2rem" }}>
-            <p>© 2025 Imperial | Find me on GitHub / LinkedIn / WhatsApp</p>
+            <p>
+                © 2025 Imperial | Find me on
+                <a href="https://github.com/SznImperial" target="_blank" rel="noopener noreferrer"> GitHub </a>/
+                <a href="https://www.linkedin.com/in/adetola-abdulkareem-ayomide-7286b3366/" target="_blank" rel="noopener noreferrer"> LinkedIn </a>/
+                <a href="wa.link/kh1t8v" target="_blank" rel="noopener noreferrer"> WhatsApp </a>
+            </p>
         </footer>
     );
 };
