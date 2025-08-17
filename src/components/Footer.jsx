94c4b73ca26063e230a80c97fbc72d4d48bdaa3f
SznImperial <adetolaabdulkareemayomide@gmail.com>
2025-08-17 07:15:28 +0100

    Links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
                 © 2025 Imperial | Find me on
                 <a href="https://github.com/SznImperial" target="_blank" rel="noopener noreferrer"> GitHub </a>/
                 <a href="https://www.linkedin.com/in/adetola-abdulkareem-ayomide-7286b3366/" target="_blank" rel="noopener noreferrer"> LinkedIn </a>/
-                <a href="wa.link/kh1t8v" target="_blank" rel="noopener noreferrer"> WhatsApp </a>
+                <a href="https://wa.me/2347072495804" target="_blank" rel="noopener noreferrer"> WhatsApp </a>
             </p>
         </footer>
     );
